947b1106a7a27fb21ee2d0c754e32d80095ea4a5
Gaurav <rajputgr513gssm@gmail.com>
2022-02-28 09:20:02 +0530

    as

--- src/App.js
+++ src/App.js
@@ -25,15 +25,15 @@ import Corseinfo from './Courseinfo';
 // import Home from './Home'
 function App() {
   const [val, setVal] = useState(0)
-  useEffect(()=>{
-    axios({
-    url:'http://localhost:8080/user/currentuser',
-    method:'POST',
-  }).then(
-    (response) => {
-      console.log(response.data);
-  })
-})
+//   useEffect(()=>{
+//     axios({
+//     url:'http://localhost:8080/user/currentuser',
+//     method:'POST',
+//   }).then(
+//     (response) => {
+//       console.log(response.data);
+//   })
+// })
 
   return (
     <stepNumber.Provider value={{
